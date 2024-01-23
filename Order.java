8a210077b1e6271b7042578d66feb92ec1f39e24
brian506 <135039872+brian506@users.noreply.github.com>
2024-01-23 22:15:11 +0900

    Update Order.java

--- Order.java
+++ Order.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name = "orders")
+@Table(name = "orders") // 테이블 명을 따로 정한 건 db안에서 order와 같은 이름이 있어서
 @Getter @Setter
 public class Order {
 
@@ -21,8 +21,10 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order") // orderItem(주인)과 일대다 관계
+    // 하나의 상품을 여러개의 상품으로 구매할 수 있으므로
     private List<OrderItem> orderItems = new ArrayList<>();
+    // 일대다 관계이므로 하인 위치인 order클래스에 컬렉션 도임
 
     @OneToOne
     @JoinColumn(name = "delivery_id")
@@ -30,6 +32,6 @@ public class Order {
 
     private LocalDateTime orderDate;
 
-    @Enumerated(EnumType.STRING)
+    @Enumerated(EnumType.STRING) //STRING이 순서에 관계 없으므로 STRING만 쓸것
     private OrderStatus status; // 주문상태[ORDER,CANCEL]
 }
