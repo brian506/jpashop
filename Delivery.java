c0ece4e3f4e96b6c2cde1a6d6878132a9e77d8bc
brian506 <135039872+brian506@users.noreply.github.com>
2024-01-25 22:04:45 +0900

    Update Delivery.java

--- Delivery.java
+++ Delivery.java
@@ -12,7 +12,9 @@ public class Delivery {
     @Column(name = "delivery_id")
     private Long id;
 
-    @OneToOne(mappedBy = "delivery") // order(주인)와 일대일 양방향관계
+    @OneToOne(mappedBy = "delivery",fetch = FetchType.LAZY) // order(주인)와 일대일 양방향관계
+    // 실무에서 모든 연관관계는 지연로딩(LAZY)로 설정
+    //~ToOne 관계는 기본이 즉시로딩이므로 직접 지연로딩으로 설정
     private Order order;
 
     @Embedded
